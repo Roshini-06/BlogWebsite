e69bd0c6ce8c54067e37cf67c15fe4b332be03b2
Roshini <jkroshini5@gmail.com>
2025-09-06 15:23:49 +0530

    Fix urls.py for Django 4+ and update settings.py

--- blog_website/settings.py
+++ blog_website/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "fallback-secret-key")
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = ["blogwebsite.onrender.com", "localhost"]
+ALLOWED_HOSTS = ["blogwebsite-3tan.onrender.com", "127.0.0.1", "localhost"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
